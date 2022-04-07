486a270a6326f1043f1560ee7cb06eeca460e109
fahinurhaquefima <fahinurhaquef@gmail.com>
2022-04-07 16:04:06 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var typed = new Typed('.element', {
+  /**
+   * @property {array} strings strings to be typed
+   * @property {string} stringsElement ID of element containing string children
+   */
+  strings: [
+    'Fima. ',
+    'Web Devloper.',
+    'Web Designer.',
+    'Graphics Designer.'
+  ],
+  stringsElement: null,
+  typeSpeed: 0,
+  startDelay: 0,
+  backSpeed: 100,
+  smartBackspace: true,
+  shuffle: false,
+  backDelay: 700,
+  fadeOut: false,
+  fadeOutClass: 'typed-fade-out',
+  fadeOutDelay: 500,
+  loop: false,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+});
+
+var typed = new Typed('.food', {
+  /**
+   * @property {array} strings strings to be typed
+   * @property {string} stringsElement ID of element containing string children
+   */
+  strings: [
+    'Burger ',
+    'Chawmin',
+    'Pizza',
+    'pasta basta',
+    'Shawrma.',
+  ],
+  stringsElement: null,
+  typeSpeed: 0,
+  startDelay: 0,
+  backSpeed: 100,
+  smartBackspace: true,
+  shuffle: false,
+  backDelay: 700,
+  fadeOut: false,
+  fadeOutClass: 'typed-fade-out',
+  fadeOutDelay: 500,
+  loop: false,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: '|',
+  autoInsertCss: true,
+});
